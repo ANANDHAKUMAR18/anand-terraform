aa39b9c7153de6c3177ccb912511f1eef60a15d0
ANANDHAKUMAR18 <101105244+ANANDHAKUMAR18@users.noreply.github.com>
2024-07-04 11:47:13 +0530

    Update main.tf

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -8,7 +8,7 @@ variable "cidr" {
 }
 
 resource "aws_key_pair" "example" {
-  key_name   = "terraform-demo-abhi"  # Replace with your desired key name
+  key_name   = "terraform-ak"  # Replace with your desired key name
   public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
 }
 
@@ -20,6 +20,13 @@ resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "sub2" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
@@ -36,10 +43,20 @@ resource "aws_route_table" "RT" {
   }
 }
 
+locals {
+  subnet_ids = {
+    "public_subnet1" = aws_subnet.sub1.id,
+    "public_Subnet2" = aws_subnet.sub2.id
+  }
+}
+
 resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.sub1.id
+  for_each = local.subnet_ids
+  subnet_id = each.value
   route_table_id = aws_route_table.RT.id
 }
+
+
 
 resource "aws_security_group" "webSg" {
   name   = "web"
@@ -72,35 +89,29 @@ resource "aws_security_group" "webSg" {
   }
 }
 
-resource "aws_instance" "server" {
+resource "aws_instance" "ak-1" {
   ami                    = "ami-0261755bbcb8c4a84"
   instance_type          = "t2.micro"
   key_name      = aws_key_pair.example.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
+  availability_zone = "us-east-1a"
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
-    host        = self.public_ip
+  tags = {
+    Name = "AK-1"
   }
+}
 
-  # File provisioner to copy a file from local to the remote EC2 instance
-  provisioner "file" {
-    source      = "app.py"  # Replace with the path to your local file
-    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
-  }
+resource "aws_instance" "ak-2" {
+  ami = "ami-0261755bbcb8c4a84"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id = aws_subnet.sub2.id
+  availability_zone = "us-east-1b"
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello from the remote instance'",
-      "sudo apt update -y",  # Update package lists (for ubuntu)
-      "sudo apt-get install -y python3-pip",  # Example package installation
-      "cd /home/ubuntu",
-      "sudo pip3 install flask",
-      "sudo python3 app.py &",
-    ]
+  tags = {
+    Name = "AK-2"
   }
 }
 
